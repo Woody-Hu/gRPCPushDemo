ba4bcb74f2de74b460db40140d1a7422400b6f5b
Woody-Hu <812791840@qq.com>
2020-02-04 15:15:47 +0800

    2020 02 04

---
+++ gRPCPush/gRPCMessage.Client/FanoutConsumerClient.cs
@@ -0,0 +1,133 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gRPCMessage.Client
+{
+    public class FanoutConsumerClient: IDisposable
+    {
+        private readonly GrpcChannel _grpcChannel;
+
+        private readonly string _address;
+
+        private readonly MessageService.MessageServiceClient _messageServiceClient;
+
+        private string _clientId = Guid.NewGuid().ToString();
+
+        private Action<string> _consumeHook;
+
+        private Task _messageTask;
+
+        private readonly CancellationToken _cancellationToken;
+
+        private bool _healthy;
+
+        private Task<bool> _replayMoveNextTask;
+
+        private Task _pingDelayTask;
+
+        private ReaderWriterLockSlim _healthyLock = new ReaderWriterLockSlim();
+
+        public bool Healthy
+        {
+            get
+            {
+                try
+                {
+                    _healthyLock.EnterReadLock();
+                    return _healthy;
+                }
+                finally
+                {
+                    _healthyLock.ExitReadLock();
+
+                }
+            }
+            set
+            {
+                try
+                {
+                    _healthyLock.EnterWriteLock();
+                    _healthy = value;
+                }
+                finally
+                {
+                    _healthyLock.ExitWriteLock();
+
+                }
+            }
+        }
+
+        public FanoutConsumerClient(string address, Action<string> consumeHook, CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+            _address = address;
+            _consumeHook = consumeHook;
+            _grpcChannel = GrpcChannel.ForAddress(_address);
+            _messageServiceClient = new MessageService.MessageServiceClient(_grpcChannel);
+            _messageTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        await WaitMessageAsync();
+                    }
+                    catch
+                    {
+                        // any cases retry
+                        Healthy = false;
+                    }
+
+                }
+            }, _cancellationToken);
+        }
+
+        private async Task WaitMessageAsync()
+        {
+            using (var call = _messageServiceClient.SendMessage(deadline: DateTime.MaxValue, cancellationToken: _cancellationToken))
+            {
+                await call.RequestStream.WriteAsync(new MessageRequest() { ClientId = _clientId });
+                Healthy = true;
+                _replayMoveNextTask = call.ResponseStream.MoveNext(_cancellationToken);
+                _pingDelayTask = Task.Delay(TimeSpan.FromSeconds(10), _cancellationToken);
+                while (true)
+                {
+                    await Task.WhenAny(_replayMoveNextTask, _pingDelayTask);
+                    if (_pingDelayTask.IsCanceled || _replayMoveNextTask.IsCanceled || _replayMoveNextTask.IsFaulted)
+                    {
+                        Healthy = false;
+                        break;
+                    }
+
+                    if (_pingDelayTask.IsCompleted)
+                    {
+                        await call.RequestStream.WriteAsync(new MessageRequest() { ClientId = _clientId });
+                        _pingDelayTask = Task.Delay(TimeSpan.FromSeconds(10), _cancellationToken);
+                    }
+
+                    if (_replayMoveNextTask.IsCompleted)
+                    {
+                        var value = call.ResponseStream.Current;
+                        _consumeHook?.Invoke(value.Data);
+                        _replayMoveNextTask = call.ResponseStream.MoveNext(_cancellationToken);
+                    } 
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_messageTask != null && !_messageTask.IsCompleted)
+            {
+                _messageTask.Dispose();
+            }
+
+            if (_grpcChannel != null)
+            {
+                _grpcChannel.Dispose();
+            }
+        }
+    }
+}
---
+++ gRPCPush/gRPCPush.Service/FanOutMessageService.cs
@@ -0,0 +1,109 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using System.Threading.Channels;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace gRPCMessage.Service
+{
+    public sealed class FanOutMessageService:MessageService.MessageServiceBase
+    {
+        private Channel<MessageReply> _channel;
+
+        private static readonly ConcurrentDictionary<string, FanOutMessageService> _keepAlivedServices = new ConcurrentDictionary<string, FanOutMessageService>();
+
+        private DateTime _lastPingTime;
+
+        private Task<bool> _pingTask;
+
+        private Task<MessageReply> _channelTask;
+
+        private string _connectionId = Guid.NewGuid().ToString();
+
+        private static readonly TimeSpan _idleTime = TimeSpan.FromMinutes(30);
+
+        public FanOutMessageService()
+        {
+            _channel = Channel.CreateUnbounded<MessageReply>();
+            _lastPingTime = DateTime.UtcNow;
+            _keepAlivedServices.GetOrAdd(_connectionId, this);
+        }
+
+        public static async Task FanoutAsync(MessageReply message)
+        {
+            var now = DateTime.UtcNow;
+            var removedKeys = new List<string>();
+            foreach (var oneServicePair in _keepAlivedServices)
+            {
+                if (now - oneServicePair.Value._lastPingTime < _idleTime)
+                {
+                    await oneServicePair.Value._channel.Writer.WriteAsync(message);
+                }
+
+                removedKeys.Add(oneServicePair.Key);
+            }
+
+            foreach (var oneKey in removedKeys)
+            {
+                _keepAlivedServices.TryRemove(oneKey, out var service);
+            }
+
+
+        }
+
+        public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
+        {
+            while (true)
+            {
+                try
+                {
+                    // remove useless connections
+                    if (_keepAlivedServices.ContainsKey(_connectionId))
+                    {
+                        break;
+                    }
+
+                    if (_pingTask == null)
+                    {
+                        _pingTask = requestStream.MoveNext(context.CancellationToken);
+                    }
+
+                    if (_channelTask == null)
+                    {
+                        _channelTask = _channel.Reader.ReadAsync(context.CancellationToken).AsTask();
+                    }
+
+                    var delayTask = Task.Delay(TimeSpan.FromSeconds(30), context.CancellationToken);
+
+                    await Task.WhenAny(_pingTask, _channelTask, delayTask);
+                    if ((_pingTask.IsFaulted || _channelTask.IsFaulted) || (_pingTask.IsCanceled || _pingTask.IsCanceled))
+                    {
+                        _keepAlivedServices.TryRemove(_connectionId, out var value);
+                        // any cases close connection
+                        break;
+                    }
+
+                    if (_pingTask.IsCompletedSuccessfully)
+                    {
+                        _lastPingTime = DateTime.UtcNow;
+                        _pingTask = requestStream.MoveNext(context.CancellationToken);
+                    }
+
+                    if (_channelTask.IsCompletedSuccessfully)
+                    {
+                        await responseStream.WriteAsync(_channelTask.Result);
+                        _channelTask = _channel.Reader.ReadAsync(context.CancellationToken).AsTask();
+                    }
+                }
+                catch
+                {
+                    _keepAlivedServices.TryRemove(_connectionId, out var value);
+                    // any cases close connection
+                    break;
+                }             
+            }
+        }
+    }
+}
